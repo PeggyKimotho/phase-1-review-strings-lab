ca6c03dba80a34826c7a7b97e90ebaddb4323420
Peggy <peggykimotho@gmail.com>
2023-06-12 09:45:13 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'Peggy';
+let welcomeMessage = 'Welcome to Flatbook, Peggy!';
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, PEGGY!'
+let shortGreeting = 'Welcome, P!'
